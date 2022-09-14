4aef419cf5cad3bd3c7071131c1a60fa4ee736e1
Luke <103580092+L-u-k-e-GIT@users.noreply.github.com>
2022-09-14 14:44:09 +0200

    Add files via upload

---
+++ share_creation.tf
@@ -0,0 +1,12 @@
+
+resource "azurerm_storage_share" "FSShare" {
+  name                 = var.MD_SHARE_NAME
+  storage_account_name = var.MD_ST_NAME
+  quota                = var.MD_SHAREQUOTA
+  
+
+}
+
+output "share_name" {
+  value = azurerm_storage_share.FSShare.name
+}
---
+++ variables.tf
@@ -0,0 +1,19 @@
+
+
+variable "MD_SHARE_NAME" { 
+ description = "Share Name"
+ type = string
+ default = ""
+}
+
+variable "MD_ST_NAME" { 
+ description = "Storage Account Name"
+ type = string
+ default = ""
+}
+
+variable "MD_SHAREQUOTA" { 
+ description = "Share Quota"
+ type = string
+ default = ""
+}
