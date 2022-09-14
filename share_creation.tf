b6a9e70d7270840a6ff95905daa386fad95d2200
Luke <103580092+L-u-k-e-GIT@users.noreply.github.com>
2022-09-14 17:26:57 +0200

    add tags

--- share_creation.tf
+++ share_creation.tf
@@ -3,7 +3,7 @@ resource "azurerm_storage_share" "FSShare" {
   name                 = var.MD_SHARE_NAME
   storage_account_name = var.MD_ST_NAME
   quota                = var.MD_SHAREQUOTA
-  
+  tags = var.MD_ALL_TAGS
 
 }
 
